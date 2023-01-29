aa0e1fc0ab61014350850e33a0a87442ab546487
Paras Roy <93031750+Paras-Roy@users.noreply.github.com>
2023-01-29 13:39:49 +0530

    flask server init

--- credit-ease/src/pages/home/form/Form.jsx
+++ credit-ease/src/pages/home/form/Form.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
 import './form.scss'
 
 const Form = () => {
+
+    const [name, setName] = useState({})
+    
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch("/api");
+                if (response.status === 200) {
+                    const data = await response.json();
+                    setName(data);
+                } else {
+                    throw new Error("An error occurred");
+                }
+            } catch (err) {
+                console.log(err);
+                setName({});
+            }
+        }
+        fetchData();
+    }, []);
+
     return ( 
         <div className="form">
-            Form
+            {name.tutorial}
         </div>
      );
 }
